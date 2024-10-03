90c3931c2ea9bb87d1f441115579789b2465567b
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-10-03 23:19:07 +0200

    about page responsive

--- src/components/SkillWrapper.jsx
+++ src/components/SkillWrapper.jsx
@@ -1,13 +1,13 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
+// import { useState } from "react";
 import SkillItem from "./SkillItem";
 
-const SkillWrapper = ({ category, skills }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const handleClick = () => setIsOpen(prev => !prev);
+const SkillWrapper = ({ category, skills, currentSkill, onClick }) => {
+    // const [isOpen, setIsOpen] = useState(false);
+    // const handleClick = () => setIsOpen(prev => !prev);
 
     return (
-        <li className={`skill__wrapper ${isOpen ? `open` : ``}`} onClick={handleClick}>
+        <li className={`skill__wrapper skill__wrapper--${category.replace(/\s+/g, '-')} ${currentSkill == category ? `open` : ``}`} onClick={() => onClick(category)}>
             <div className="category__wrapper">
                 <h4 className="skill__category emph">{category}</h4>
                 <svg className="toggle__img" width="16" height="9" viewBox="0 0 16 9" fill="none">
@@ -25,7 +25,9 @@ const SkillWrapper = ({ category, skills }) => {
 
 SkillWrapper.propTypes = {
     category: PropTypes.string.isRequired,
-    skills: PropTypes.array.isRequired
+    skills: PropTypes.array.isRequired,
+    currentSkill: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
 };
 
 export default SkillWrapper
--- src/components/SkillsetWrapper.jsx
+++ src/components/SkillsetWrapper.jsx
@@ -1,15 +1,22 @@
 import PropTypes from "prop-types";
 import SkillWrapper from "../components/SkillWrapper";
+import { useState } from "react";
 
 const SkillsetWrapper = ({ type, skills }) => {
     const suffix = type.endsWith('e') ? 'r' : 'er';
+    const [skill, setSkill] = useState("");
+
+    const handleSkillClick = newSkill => {
+        if (newSkill == skill) setSkill("");
+        else setSkill(newSkill)
+    }
 
     return (
         <section className={`skillset__wrapper skillset__wrapper--${type}`}>
             <h3 className="skillset__title title--wrapped">Part <em className="emph h2">{type + suffix}</em></h3>
             <ul className={`skillset__skills skillset__skills--${type}`}>
                 {Object.entries(skills).map(([category, skillList]) => (
-                    <SkillWrapper key={category} category={category} skills={skillList} />
+                    <SkillWrapper key={category} category={category} skills={skillList} currentSkill={skill} onClick={v => handleSkillClick(v)} />
                 ))}
             </ul>
         </section>
